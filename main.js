3e856a19b2ccf770f01795db9af1059c81e95ac0
Sig Nin <signin@email.com>
2019-10-09 00:16:26 -0700

    add a button to the page and react when it's clicked

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("Hello, world! -- dom-manipulation");
+
+let displayPromptButton = document.getElementById('displayPromptButton');
+displayPromptButton.addEventListener('click', function() {
+  console.log('clicked!');
+});
