0587d45b63411c5d1486824cf60c83c0cf1b2a7f
Sig Nin <signin@email.com>
2019-10-09 00:29:46 -0700

    show user response on the page

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ console.log("Hello, world! -- dom-manipulation");
 
 document.addEventListener('DOMContentLoaded', function() {
   let displayPromptButton = document.getElementById('displayPromptButton');
+  let view = document.getElementById('color');
   displayPromptButton.addEventListener('click', function() {
     console.log('clicked!');
     let input = prompt('What is your favorite color?');
     console.log('input is ', input);
+    view.innerHTML = input;
   });
 });
