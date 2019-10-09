f691f2a3beb41bedf6eee14edf2002d24ea11049
Sig Nin <signin@email.com>
2019-10-09 00:48:42 -0700

    show favorite colors in a list

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ console.log("Hello, world! -- dom-manipulation");
 
 document.addEventListener('DOMContentLoaded', function() {
   let displayPromptButton = document.getElementById('displayPromptButton');
-  let view = document.getElementById('color');
+  let viewInputColor = document.getElementById('color');
+  let viewColorList = document.getElementById('colorList');
   displayPromptButton.addEventListener('click', function() {
     console.log('clicked!');
     let input = prompt('What is your favorite color?');
     console.log('input is ', input);
-    view.innerHTML = input;
+    viewInputColor.innerHTML = "Your favorite color is: " + input;
+    let listItem = document.createElement('li');
+    listItem.innerHTML = input;
+    viewColorList.appendChild(listItem);
   });
 });
