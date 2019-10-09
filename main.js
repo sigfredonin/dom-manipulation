6bd46ece825624ff0254bdaae93090d2acfa0d2d
Sig Nin <signin@email.com>
2019-10-09 00:24:30 -0700

    added prompt for favorite color on button click

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let displayPromptButton = document.getElementById('displayPromptButton');
   displayPromptButton.addEventListener('click', function() {
     console.log('clicked!');
+    let input = prompt('What is your favorite color?');
+    console.log('input is ', input);
   });
 });
